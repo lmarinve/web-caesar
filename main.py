66f18714de686ef1a426762cfff10f5d5be49904
Ubuntu <ubuntu@ip-172-31-0-202.us-east-2.compute.internal>
2019-09-13 00:31:36 +0000

    caesar.py implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEGUG'] = True
@@ -16,33 +17,50 @@ form = '''<!DOCTYPE html>
                 font: 16px sans-serif;
                 border-radius: 10px;
             }
-            textarea {
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
+            textarea {{
+                width: 300px;
+                height: 150px;
+                padding: 12px 20px;
+                box-sizing: border-box;
+                border: 2px solid #ccc;
+                border-radius: 4px;
+                background-color: #f8f8f8;
+                font-size: 16px;
+                resize: none;
+            }}
+            div.div1 {
+            
+                width:540px;
+                margin: 16px 16px;
             }
         </style>
     </head>
     <body>
       <!-- create your form here -->
-      <form action=/hello method="post">
-        <label for="rot">Rotate by::</label>
-        <input id="rot" type="text" name="rot" />
-        <input id="text" type="textarea" name="text" placeholder="" />
+      <form action=/ method="post">
+        <div class ="div1">
+        <label for="rot">Rotate by:</label>
+        <input id="rot" type="text" name="rot" value="0" />
+        </div>
+        <textarea name="text" {0} rows="3" cols="35"></textarea>
         <input type="submit" value="Submit" />
       </form>
     </body>
 </html>
-view raw'''
+'''
+
+
 @app.route('/')
 def index():
     return form
 
 
-@app.route('/hello', methods=['POST'])
-def hello():
-    first_name = request.form['first_name']
-    return '<h1>Hello, ' + first_name + '</h1>'
+@app.route('/', methods=['POST'])
+def encrypt():
+    rec_rot = int(request.form['rot'])
+    rec_text = request.form['text']
+    rotated = rotate_string(rec_text,rec_rot)
+    return '<h1> ' + rotated + '</h1>'
 
 
 if __name__ == '__main__':
